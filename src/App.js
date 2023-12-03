1f5053044d88a695ff2aad8b52d4aa35b9b11283
toup162 <toup162@users.noreply.github.com>
2023-12-03 02:04:08 -0500

    Add boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { MapContainer, TileLayer } from 'react-leaflet';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<MapContainer center={[0,0]} zoom={3}>
+        <TileLayer
+          attribution='test'
+          url='../rift/{z}/{y}/{x}.jpg'
+        />
+      </MapContainer>
+		</div>
+	);
 }
 
 export default App;
